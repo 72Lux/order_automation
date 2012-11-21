16feb8a6b4de901f5d6cad8f4fd7f448ae60978f
Joe Ekiert <joe@72lux.com>
2012-11-21 16:23:23 -0500

    really messy commit, going to clean it up, but wanted this in the history

--- neiman.js
+++ neiman.js
@@ -1,6 +1,6 @@
 // contains a select for size
 // will need one for color
-var url = 'http://www.neimanmarcus.com/p/Ferragamo-Reversible-Leather-Belt-Black-Brown/prod154930114_cat13230731__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.service%253FitemId%253Dcat13230731%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod154930114&cmCat=product';
+var url = 'http://www.neimanmarcus.com/p/Yves-Saint-Laurent-Patent-Leather-Pointed-Toe-Pump-Pumps/prod146820188_cat39980735__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.jsp%253FitemId%253Dcat39980735%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod146820188&cmCat=product';
 casper = require('casper').create({
   //verbose: true,
   //logLevel: "debug"
@@ -13,27 +13,41 @@ casper.start(url, function () {
   casper.test.assertExists('#topAddToCartButton');
 });
 
-// select the size and assert that item is in stock
+// STEP 1: Select the size and assert that the item is in stock
 casper.then(function () {
-  this.click('.lineItemOptionSelect select:nth-of-type(1)');
-}).waitFor();
-
-// get something here to make sure the select options are there
-
-casper.then(function () {
-  this.click('.lineItemOptionSelect select:nth-of-type(1) option[value="34"]')
+  this.click('.lineItemOptionSelect select:nth-of-type(1) option[value="38.5B/8.5B"]');
 }).waitFor(function checkSizeAvailability () {
   return this.evaluate(function () {
-    return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]').length;
+    return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]').length || document.querySelectorAll('.lineItemOptionSelect select:nth-of-type(1) option').length > 1;
   });
 }, function then () {
-  casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]');
+  casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]','Product is in stock at the indicated size!');
 }, function timeout () {
-  this.echo('timed out/out of stock');
-  casper.test.assertExists('.prodStatus img[src="/common/images/shim.gif"]');
+  this.echo('timed out');
+
+  // this shouldn't be here, because when a size is chosen
+  // then a color is 'selected' and THEN the inventory test
+  // is performed.
+  casper.test.assertExists('.prodStatus img[src="/common/images/shim.gif"]','Product does not appear in stock at that size');
 });
 
+// STEP 2: Select the color and assert that the item is in stock
+//casper.then(function () {
+//  this.click('.lineItemOptionSelect select:nth-of-type(2) option[value="BLACK"]');
+//}).waitFor(function checkColorAvailability () {
+//  return this.evaluate(function () {
+//    return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]').length;
+//  });
+//}, function then () {
+//  casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]','Product is in stock at the indicated size AND color!');
+//}, function timeout () {
+//  this.echo('timed out');
+//});
 
+// STEP 3: Check if that shim is still there (it shouldn't if in stock)
+casper.then(function () {
+  casper.test.assertExists('.prodStatus img[src="/common/images/shim.gif"]','Product does not appear in stock');
+});
 
 casper.run(function () {
   this.test.renderResults(true);
