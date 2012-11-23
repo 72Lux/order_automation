94590d26e4091480275c50e6ef810279d8e0b67e
Joe Ekiert <joe.ekiert@gmail.com>
2012-11-23 15:37:23 -0500

    cleaned up code, have two url snippets - one for testing multiple sizes, single color, another for testing multiple sizes and multiple colors

--- neiman.js
+++ neiman.js
@@ -1,4 +1,9 @@
-var url = 'http://www.neimanmarcus.com/p/Yves-Saint-Laurent-Patent-Leather-Pointed-Toe-Pump-Pumps/prod146820188_cat39980735__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.jsp%253FitemId%253Dcat39980735%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod146820188&cmCat=product';
+// product that contains multiple sizes and multiple colors for size select option[value="38.5B/8.5B"]
+//var url = 'http://www.neimanmarcus.com/p/Yves-Saint-Laurent-Patent-Leather-Pointed-Toe-Pump-Pumps/prod146820188_cat39980735__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.jsp%253FitemId%253Dcat39980735%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod146820188&cmCat=product';
+
+// product that contains multiple sizes and no color options
+var url = 'http://www.neimanmarcus.com/p/Prada-Wing-Tip-Chelsea-Boot-Boots/prod146820012_cat6750735__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.jsp%253FitemId%253Dcat6750735%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod146820012&cmCat=product';
+
 casper = require('casper').create({
   //verbose: true,
   //logLevel: "debug"
@@ -8,31 +13,36 @@ casper.start(url, function () {
   casper.test.assertExists('#topAddToCartButton');
 });
 
-// STEP 1: Size & Color
-// NOTE: Need to add select color given a SINGLE size
+// select size first
 casper.then(function () {
-  this.click('.lineItemOptionSelect select:nth-of-type(1) option[value="38.5B/8.5B"]');
+  this.click('.lineItemOptionSelect select:nth-of-type(1) option[value="6.5/7.5D"]');
 });
-casper.waitFor(function checkColorsForSize () {
+
+casper.waitFor(function () {
   return this.evaluate(function () {
-    return document.querySelectorAll('.lineItemOptionSelect select:nth-of-type(2) option').length > 1;
+    return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]');
   });
 }, function then () {
-  // in stock in that size and there are multiple colors available
-  this.click('.lineItemOptionSelect select:nth-of-type(2) option[value="BLACK"]');
-  casper.waitFor(function checkStockForColor () {
-    return this.evaluate(function () {
-      return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]').length;
-    });
-  }, function then () {
-    casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]','Product is in stock at selected size and color!');
-  }, function timeout () {
-    this.echo('done-zo, no products available');
-  });
+  this.echo('product available in that size');
 }, function timeout () {
-  // in stock in that size, no other color options
-  casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]','Product is in stock with no other color choices!');
+  this.echo('not available, may need to select a color');
+  casper.test.assertExists('.prodStatus img[src="/category/images/prod_stock1.gif"]');
 });
+
+// select color
+//casper.then(function () {
+//  this.click('.lineItemOptionSelect select:nth-of-type(2) option[value="BLACK"]');
+//  // check if item is in stock, if not then check for colors
+//  casper.waitFor(function checkForSizeAvailability () {
+//    return this.evaluate(function () {
+//      return document.querySelectorAll('.prodStatus img[src="/category/images/prod_stock1.gif"]').length;
+//    });
+//  }, function then () {
+//    this.echo('product available in that size and color');
+//  }, function timeout () {
+//    this.echo('dont know whats going on');
+//  });
+//});
 
 casper.run(function () {
   this.test.renderResults(true);
