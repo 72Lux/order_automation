d892884eb0cbaeb8dafbbd7cbeaed5cc167acec0
Joe Ekiert <joe@72lux.com>
2012-11-30 12:46:38 -0500

    my own little function for taking .capture shots, accepts arg for renaming file

--- neiman.js
+++ neiman.js
@@ -1,3 +1,14 @@
+function picit(arg) {
+  arg = arg || 'results.png';
+  casper.test.comment('Cheeeeeeese!');
+  casper.capture(arg, {
+    top: 0,
+    left: 0,
+    width: 1024,
+    height: 1024
+  });
+}
+
 // product that contains multiple sizes and multiple colors for size select option[value="38.5B/8.5B"]
 //var url = 'http://www.neimanmarcus.com/p/Yves-Saint-Laurent-Patent-Leather-Pointed-Toe-Pump-Pumps/prod146820188_cat39980735__/?icid=&searchType=EndecaDrivenCat&rte=%252Fcategory.jsp%253FitemId%253Dcat39980735%2526pageSize%253D30%2526No%253D0%2526refinements%253D&eItemId=prod146820188&cmCat=product';
 
