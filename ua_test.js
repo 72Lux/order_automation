b75f5036dff2f62d1016ca81877898944f6b6519
Vivek Dhar <vivekdhar@gmail.com>
2013-04-11 08:36:26 -0700

    ua_test updates.

--- ua_test.js
+++ ua_test.js
@@ -1,3 +1,5 @@
+//provide cookie file at cl : casperjs --cookies-file=2.txt ua_test.js
+
 require("utils");
 
 // capture a snapshot
@@ -30,7 +32,7 @@ var casper = require("casper").create({
 
 casper.start();
 
-casper.userAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5');
+// casper.userAgent('Default UA');
 
 casper.thenOpen('http://click.linksynergy.com/link?id=v9jIDxMZD/A&u1=&type=15&offerid=279712&murl=http%3A%2F%2Fwww.neimanmarcus.com%2Fp%2FNeiman-Marcus-Contour-Jean-Belt-Black%2Fprod150910006_cat4300731__%2F%3Ficid%3D%26searchType%3DEndecaDrivenCat%26rte%3D%25252Fcategory.service%25253FitemId%25253Dcat4300731%252526pageSize%25253D30%252526No%25253D120%252526refinements%25253D%26eItemId%3Dprod150910006%26cmCat%3Dproduct', function() {
   this.wait(2000, function() {
@@ -38,7 +40,9 @@ casper.thenOpen('http://click.linksynergy.com/link?id=v9jIDxMZD/A&u1=&type=15&of
   });
 });
 
-casper.thenOpen('http://shop.nordstrom.com/s/kate-spade-new-york-dellas-sunglasses/3491815?origin=category&contextualcategoryid=0&fashionColor=Mint&resultback=0', function() {
+casper.userAgent('Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5');
+
+casper.thenOpen('http://click.linksynergy.com/link?id=v9jIDxMZD/A&u1=&type=15&offerid=279712&murl=http%3A%2F%2Fwww.neimanmarcus.com%2Fp%2FNeiman-Marcus-Contour-Jean-Belt-Black%2Fprod150910006_cat4300731__%2F%3Ficid%3D%26searchType%3DEndecaDrivenCat%26rte%3D%25252Fcategory.service%25253FitemId%25253Dcat4300731%252526pageSize%25253D30%252526No%25253D120%252526refinements%25253D%26eItemId%3Dprod150910006%26cmCat%3Dproduct', function() {
   this.wait(2000, function() {
     picit(new Date().getTime());
   });
