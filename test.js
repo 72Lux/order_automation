5c611b23bfc933ba4d99a35c3ef55dba85daa521
Vivek Dhar <vivekdhar@gmail.com>
2013-04-18 21:39:08 -0700

    changing size on test item.

--- test.js
+++ test.js
@@ -107,7 +107,7 @@ var lineItems = [];
 var item0 = {
     name: "Contour Jean Belt, Black",
     affiliate_url: 'http://click.linksynergy.com/link?id=v9jIDxMZD/A&u1=&type=15&offerid=279712&murl=http%3A%2F%2Fwww.neimanmarcus.com%2Fp%2FNeiman-Marcus-Contour-Jean-Belt-Black%2Fprod150910006_cat4300731__%2F%3Ficid%3D%26searchType%3DEndecaDrivenCat%26rte%3D%25252Fcategory.service%25253FitemId%25253Dcat4300731%252526pageSize%25253D30%252526No%25253D120%252526refinements%25253D%26eItemId%3Dprod150910006%26cmCat%3Dproduct',
-    size: 'X-SMALL',
+    size: 'SMALL',
     color: 'BLACK',
     qty: 1
   };
